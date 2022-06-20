ad1ca46c12db68135648bcfea375db33dad771ae
marccoady <68036003+marccoady@users.noreply.github.com>
2022-06-20 14:30:13 -0700

    Add files via upload

---
+++ whatsyourname.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3.7
+
+firstName = input("Enter your First Name: ")
+lastName = input("Enter your last Name: ")
+print("Hello " + firstName + " " + lastName + "!" + " You just delved into Python.")
