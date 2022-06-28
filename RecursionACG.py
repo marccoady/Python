a57f6a599ab24a50b20c50d75be2fd000ad877a6
EC2 Default User <ec2-user@ip-172-31-94-198.ec2.internal>
2022-06-28 21:42:48 +0000

    Fibonnaci exercise

--- RecursionACG.py
+++ RecursionACG.py
@@ -5,6 +5,7 @@ def fib(n):
         return 0
     elif n == 1:
         return 1
+        
     return fib(n - 2) + fib(n - 1)
     
 item_to_calculate = int(input("What Fibonnaci item would you like to calculate? "))
