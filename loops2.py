46c23c5db9357b50165de9e790d11c9998a4f8bf
EC2 Default User <ec2-user@ip-172-31-94-198.ec2.internal>
2022-06-29 03:06:07 +0000

    Update for loops da loops

--- loops2.py
+++ loops2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.7
 
-if __name__ == '__main__':
-    n = int(input())
-    if n > 1 and n < 20 :
-        print(n)     
+n = int(input("Enter a number: "))
+if n > 1 and n < 20:
+    print(n)
+    
