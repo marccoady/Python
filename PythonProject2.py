ae76275545e8db47940188bf0e5bed18f52b14d3
EC2 Default User <ec2-user@ip-172-31-94-198.ec2.internal>
2022-07-02 19:09:19 +0000

    Random EC2 Name generator

--- PythonProject2.py
+++ PythonProject2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3.7
 
 # This Script can be used to create Random EC2 Names
-
-# Import modules to generate random strings
+# Import modules to generate random strings and numbers
 
 import random
 import string
@@ -13,9 +12,34 @@ while True:
     DeptName = input("Enter your Department: ")
     Department = ['Accounting', 'accounting', 'Marketing', 'marketing', 'FinOps', 'finops']
     if DeptName not in Department:
-        print("Not Authorized to use this tool!")
+        print("Not authorized to use this tool!")
         exit()
     else:
         break 
     
-ec2_ammount = input("How many random EC2 names do you want generated? ")
+# Ask for how many EC2 names needed
+    
+ec2s = input("How many random EC2 instance names do you want created? ")
+ec2s = int(ec2s)
+for _ in range(ec2s):
+    
+# Generate the EC2 Instance names using the inputed information and random modules
+# Define the characters of the random output that will be generated
+
+    def randomstring(length=2, uppercase=True, lowercase=True, numbers=True):
+        character_set = ''
+        
+        if uppercase:
+            character_set += string.ascii_uppercase
+        if lowercase:
+            character_set += string.ascii_lowercase
+        if numbers:
+            character_set += string.digits
+        
+        return ''.join(random.choice(character_set) for i in range(length))
+    rs = randomstring(4)
+    
+#Putting it all together and printing the results
+
+    print("EC2 Instance Name: ")
+    print(DeptName,'_', rs)
