fa1b569978533ca9ea264c0c8a52bff2734d92cc
EC2 Default User <ec2-user@ip-172-31-94-198.ec2.internal>
2022-07-04 18:12:42 +0000

    AWs Workshop variables

---
+++ AWSwkshopvarialbes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.7
+
+
+a_str = "This is an example of a string in quotes" # In python the word string is abbreviated to str
+my_float = 5.5
+an_interger = 5 # integer is abbreviated to int
+shopping_list = ["apples", "oranges", "pears"]
+a_dict = {"userID": "JBloggs"} # dictionary is abbreviated to dict
+my_var = another_variable # variable is abbreviated to var
+test_funtion = my_function() #function is abbreviated to func
+example_tuple = ("apple", "orange", "pear")
+boolean_values = True # boolean is abbreviated to bool
