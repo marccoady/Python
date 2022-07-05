8167635c83431f22bd368cb3b1b36d036a8e66b6
EC2 Default User <ec2-user@ip-172-31-94-198.ec2.internal>
2022-07-05 21:37:29 +0000

    Updating deleting section

--- S3upload_list_delete.py
+++ S3upload_list_delete.py
@@ -13,22 +13,22 @@ import boto3
     
 #How to upload multiple files
 
-# import os
+#import os
 #import glob
 
-# cwd=os.getcwd()
+# cwd=os.getcwd() # optional
 
-# cwd=cwd+"/upload"
+# cwd=cwd+"/upload" # optional
 #files=glob.glob("*.png")
 
 #files
 
 #for file in files:
-    #s3_resource=boto3.client("s3")
+  #  s3_resource=boto3.client("s3")
     #s3_resource.upload_file(
-    #Filename=file,
-    #Bucket="mcoadyfirstbotobucket2",
-    #Key=file.split("/")[-1])
+   # Filename=file,
+   # Bucket="mcoadyfirstbotobucket2",
+    # Key=file.split("/")[-1])
     
     
 # How to list S3 Ojbects
@@ -39,4 +39,24 @@ import boto3
 
 # for object in objects:
 #    print(object["Key"])
+
+# How to delete objects
+
+# Single object
+
+# import glob
+
+# s3_resource=boto3.client("s3")
+
+# s3_resource.delete_object(Bucket='mcoadyfirstbotobucket2', Key='upload.png')
+
+# Delete multiple objects
+
+# objects=s3_resource.list_objects(Bucket="mcoadyfirstbotobucket2")["Contents"]
+
+# for object in objects:
+    # response=s3_resource.delete_object(Bucket='mcoadyfirstbotobucket2', Key=object["Key"])
+    # print(response)
+
+
 
