2e0c54cb3214ebc3873db471e251f06fa70b89c6
marccoady <marccoady@outlook.com>
2022-07-08 00:26:33 +0000

    Updating Ec2 file

--- Getec2instanceIds.py
+++ Getec2instanceIds.py
@@ -3,15 +3,8 @@
 import boto3
 ec2_client=boto3.client("ec2")
 x=ec2_client.describe_instances()
-
-x.keys()
-
-len(x["Reservations"])
-
 data=x["Reservations"][0]
-
 data_instance=data["Instances"]
-
 for i in range (len(data_instance)):
     print(f"instance id is {data_instance[i]['InstanceId']}")
 
